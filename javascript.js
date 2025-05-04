c46bb05ab5d82eabf5df20ee9a8e558b1ed51756
Clifford <cliffordatuchukwu92@gmail.com>
2025-05-04 11:47:48 +0200

    Adding comments

--- javascript.js
+++ javascript.js
@@ -1,12 +1,15 @@
+//Looping through the tasks, incrementing the task each time until it reaches it has reached the second one
 for (let i = 1; i <= 2; i++) {
   let title = window.prompt(`Enter task ${i} title: `);
   let description = window.prompt(`Enter task ${i} description: `);
+  //Creating the variables outside of the while loop to prevent any issues
   let status;
   let lowercaseStatus;
 
+  //Asking for a status until a valid one is entered
   while (true) {
     status = window.prompt(`Enter the status of task ${i}: `);
-    lowercaseStatus = status.toLowerCase();
+    lowercaseStatus = status.toLowerCase(); //Turns any input given by user into lowercase
 
     if (
       lowercaseStatus === "todo" ||
@@ -20,8 +23,7 @@ for (let i = 1; i <= 2; i++) {
   }
 
   console.log(`Title: ${title}, Status ${lowercaseStatus}`);
-
   if (lowercaseStatus !== "done") {
-    console.log("No tasks complete, let's get to work!");
+    console.log("No tasks complete, let's get to work!"); //A message that appears in the console if none of the tasks are labelled as "done"
   }
 }
